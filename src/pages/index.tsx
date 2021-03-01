f089d9d90735c37cedeac6cdfc6a1a5ad33e00f3
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-03-01 15:20:34 -0300

    feat(componente, styles global and component)

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ExperienceBar } from "../components/ExperienceBar";
+
+export default function Home() {
+  return (
+    <div className='container' >
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet"/>
+    
+      <ExperienceBar/>
+    </div>    
+  )
+}
